4729acbe9de734a8c27734ea470cef968ba56db5
dev-taosif-net <dev.taosif.net@gmail.com>
2026-04-06 14:57:11 +0600

    Initialize modular monolith solution and API project
    Set up solution structure with Bootstrapper, Modules, and Shared folders. Added a new ASP.NET Core Web API project targeting .NET 10.0, including basic Program.cs, appsettings files, and launch settings for development.

---
+++ src/Bootstrapper/API/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add Service To the container
+
+#endregion
+
+var app = builder.Build();
+#region Request pipeline
+
+#endregion
+app.Run();
