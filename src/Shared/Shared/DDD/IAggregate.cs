9f690c3913dc67ed567f3162ac9b2bff52d4ed6a
dev-taosif-net <dev.taosif.net@gmail.com>
2026-04-21 16:19:44 +0600

    Add domain-driven design (DDD) base classes: Entity, Aggregate, IAggregate, IDomainEvent; include MediatR package

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,11 @@
+namespace Shared.DDD;
+
+public interface IAggregate<T> : IEntity<T>, IAggregate
+{
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
