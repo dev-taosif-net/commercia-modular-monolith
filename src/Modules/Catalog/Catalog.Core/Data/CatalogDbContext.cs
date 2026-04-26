8e60e71241cd1baf3c5e01cf824c28077e3f90d2
dev-taosif-net <dev.taosif.net@gmail.com>
2026-04-26 11:42:30 +0600

    Add CatalogDbContext and Product configuration for Entity Framework Core; update appsettings and project references

---
+++ src/Modules/Catalog/Catalog.Core/Data/CatalogDbContext.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+namespace Catalog.Core.Data;
+
+public class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products => Set<Product>();
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("catalog");
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());        
+        base.OnModelCreating(builder);
+    }
+}
--- src/Modules/Catalog/Catalog.Core/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog.Core/GlobalUsings.cs
@@ -1,2 +1,4 @@
 global using Shared.DDD;
 global using Catalog.Core.Products.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
