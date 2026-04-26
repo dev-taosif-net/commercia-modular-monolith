86bb025c9164246507d5dad3a0c6bf66ca010e24
dev-taosif-net <dev.taosif.net@gmail.com>
2026-04-26 12:02:57 +0600

    Refactor CatalogModule to add service registration and pipeline configuration; update GlobalUsings for Catalog.Core.Data

--- src/Modules/Catalog/Catalog.Core/CatalogModule.cs
+++ src/Modules/Catalog/Catalog.Core/CatalogModule.cs
@@ -9,11 +9,32 @@ public static class CatalogModule
     public static IServiceCollection AddCatalogModule(this IServiceCollection services,
         IConfiguration configuration)
     {
+        // Add services to the container.
+
+        // Api Endpoint services
+
+        // Application Use Case services       
+
+        // Data - Infrastructure services
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionString));
+
         return services;
     }
-    
+
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
     {
+        // Configure the HTTP request pipeline.
+
+        // 1. Use Api Endpoint services
+
+        // 2. Use Application Use Case services
+
+        // 3. Use Data - Infrastructure services  
+
+
         return app;
     }
 }
--- src/Modules/Catalog/Catalog.Core/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog.Core/GlobalUsings.cs
@@ -2,3 +2,4 @@ global using Shared.DDD;
 global using Catalog.Core.Products.Entities;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Catalog.Core.Data;
